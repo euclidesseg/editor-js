87d844d3bb92f3b499ed721dc63e04f1e1f7124f
Euclides Pérez <euder0206@gmail.com>
2026-01-29 17:35:31 -0500

    se configura y se pueba la funcionalidad con el example proveeido por prosemirror

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ import { schema } from "https://esm.sh/prosemirror-schema-basic";
 import { addListNodes } from "https://esm.sh/prosemirror-schema-list";
 import { exampleSetup } from "https://esm.sh/prosemirror-example-setup";
 
+// EditorState → Guarda TODO el estado del editor (documento, selección, plugins)
+// EditorView → Es el editor visual que se renderiza en el DOM
+// Schema → Define qué tipos de nodos y marcas existen (párrafo, heading, bold, etc.)
+// DOMParser → Convierte HTML → documento interno de ProseMirror
+// schema-basic → Esquema base (párrafos, headings, bold, italic…)
+// addListNodes → Agrega listas (ul, ol, li) al schema
+// exampleSetup → Instala plugins de DEMO (menú, teclas, history, atajos, etc.)
+
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
 const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block","li", "ul", "ol"),
   marks: schema.spec.marks
 })
 
